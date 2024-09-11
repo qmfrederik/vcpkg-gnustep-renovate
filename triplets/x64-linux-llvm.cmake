6e5a0228ae40f9b23ee568f6f5747a9f1ec71812
Frederik Carlier <frederik.carlier@keysight.com>
2024-09-11 22:18:36 +0200

    Add libobjc2 port

---
+++ triplets/x64-linux-llvm.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+# Configure toolchain
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchains/x64-linux-llvm.toolchain.cmake")
+
+set(VCPKG_FIXUP_ELF_RPATH ON)
