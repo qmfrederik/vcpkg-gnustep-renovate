9450a0ac018225279be67c7d89ec5fe07556ed64
Frederik Carlier <frederik.carlier@keysight.com>
2024-09-12 12:42:13 +0000

    libiconv: use clang-cl

--- triplets/x64-windows-llvm.cmake
+++ triplets/x64-windows-llvm.cmake
@@ -5,7 +5,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 # Configure toolchain
 # Some ports need to be built with clang-cl instead of clang.  For example,
 # libffi uses libtool, which pass msvc-style arguments to the linker (-EXPORT:symbol)
-if(PORT MATCHES "libffi")
+if(PORT MATCHES "^(libffi|libiconv)$")
     set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchains/x64-windows-clangcl.toolchain.cmake")
 else()
     set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchains/x64-windows-llvm.toolchain.cmake")
