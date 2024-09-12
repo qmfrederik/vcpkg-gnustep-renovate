fc55d79863b424a73745eccc772060bb23e6337e
Frederik Carlier <frederik.carlier@keysight.com>
2024-09-12 12:04:08 +0000

    Enable clang-cl for specific ports

--- triplets/x64-windows-llvm.cmake
+++ triplets/x64-windows-llvm.cmake
@@ -3,5 +3,12 @@ set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CRT_LINKAGE dynamic)
 
 # Configure toolchain
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchains/x64-windows-llvm.toolchain.cmake")
+# Some ports need to be built with clang-cl instead of clang.  For example,
+# libffi uses libtool, which pass msvc-style arguments to the linker (-EXPORT:symbol)
+if(PORT MATCHES "libffi")
+    set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchains/x64-windows-clangcl.toolchain.cmake")
+else()
+    set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchains/x64-windows-llvm.toolchain.cmake")
+endif()
+
 set(VCPKG_LOAD_VCVARS_ENV ON)
