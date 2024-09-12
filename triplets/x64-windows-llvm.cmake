6aa376ac4375e0b1f0d2ad6046d631451a44a048
Frederik Carlier <frederik.carlier@keysight.com>
2024-09-12 13:20:01 +0000

    Add workaround for libzma

--- triplets/x64-windows-llvm.cmake
+++ triplets/x64-windows-llvm.cmake
@@ -11,4 +11,11 @@ else()
     set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchains/x64-windows-llvm.toolchain.cmake")
 endif()
 
+# Port-specific fixes which we should remove over time
+if(PORT MATCHES "liblzma")
+    # Pretend to be msvc, as otherwise the build system will default to GCC-style semantics (and we're using clang)
+    # https://github.com/tukaani-project/xz/blob/68c54e45d042add64a4cb44bfc87ca74d29b87e2/CMakeLists.txt#L1389
+    set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DMSVC=1")
+endif()
+
 set(VCPKG_LOAD_VCVARS_ENV ON)
