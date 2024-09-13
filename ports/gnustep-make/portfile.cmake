2b4836649ea8f168d8e65e91030f626dd01a3a20
Frederik Carlier <frederik.carlier@keysight.com>
2024-09-13 22:32:29 +0200

    Keep separate Makefiles structure in /debug/share/GNUstep

--- ports/gnustep-make/portfile.cmake
+++ ports/gnustep-make/portfile.cmake
@@ -19,11 +19,12 @@ vcpkg_configure_gnustep(
 vcpkg_install_gnustep()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 # Empty directories
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/GNUstep/Makefiles/Additional")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share/GNUstep/Makefiles/Additional")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/GNUstep/Makefiles/Auxiliary")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share/GNUstep/Makefiles/Auxiliary")
 
 # Utilities which are not used in the build process and contain absolute path
 file(REMOVE "${CURRENT_PACKAGES_DIR}/bin/debugapp")
@@ -43,7 +44,9 @@ file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/etc/GNUstep/GNUstep.conf")
 # We don't use .csh files in the build process. They contain absolute paths;
 # remove them.
 file(REMOVE "${CURRENT_PACKAGES_DIR}/share/GNUstep/Makefiles/GNUstep.csh")
+file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/share/GNUstep/Makefiles/GNUstep.csh")
 file(REMOVE "${CURRENT_PACKAGES_DIR}/share/GNUstep/Makefiles/filesystem.csh")
+file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/share/GNUstep/Makefiles/filesystem.csh")
 
 # Fix path in gnustep-config, GNUstep.sh, GNUstep.conf and filesystem.sh
 function(z_vcpkg_fixup_gnustep_path file find replace)
@@ -62,19 +65,21 @@ if (CMAKE_HOST_WIN32)
 endif()
 
 z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/bin/gnustep-config" "${vcpkg_package_prefix}" "$(realpath \"$(dirname $0)/../\")")
-
-if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-    # ./debug/share does not exist, so redirect to ./share; but fixup the other paths relative to ./debug
-    z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/debug/bin/gnustep-config" "${vcpkg_package_prefix}/debug/share" "$(realpath \"$(dirname $0)/../../share/\")")
-    z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/debug/bin/gnustep-config" "${vcpkg_package_prefix}" "$(realpath \"$(dirname $0)/../\")")
-endif()
+z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/debug/bin/gnustep-config" "${vcpkg_package_prefix}/debug" "$(realpath \"$(dirname $0)/../\")")
 
 # because GNUstep.sh is sourced, use ${BASH_SOURCE[0]}.  This is less portable but works.
 z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/share/GNUstep/Makefiles/GNUstep.sh" "${vcpkg_package_prefix}" "$(realpath \"$(dirname \${BASH_SOURCE[0]})/../../../\")")
+z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/debug/share/GNUstep/Makefiles/GNUstep.sh" "${vcpkg_package_prefix}/debug" "$(realpath \"$(dirname \${BASH_SOURCE[0]})/../../../\")")
+
 z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/share/GNUstep/Makefiles/GNUstep-reset.sh" "${vcpkg_package_prefix}" "$(realpath \"$(dirname \${BASH_SOURCE[0]})/../../../\")")
+z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/debug/share/GNUstep/Makefiles/GNUstep-reset.sh" "${vcpkg_package_prefix}/debug" "$(realpath \"$(dirname \${BASH_SOURCE[0]})/../../../\")")
+
 z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/share/GNUstep/Makefiles/filesystem.sh" "${vcpkg_package_prefix}" "$(realpath \"$(dirname \${BASH_SOURCE[0]})/../../../\")")
+z_vcpkg_fixup_gnustep_path("${CURRENT_PACKAGES_DIR}/debug/share/GNUstep/Makefiles/filesystem.sh" "${vcpkg_package_prefix}/debug" "$(realpath \"$(dirname \${BASH_SOURCE[0]})/../../../\")")
 
 # gnustep-make has no headers
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
+# The makefiles used by GNUstep go in share, and are different for the release and debug configuration
+set(VCPKG_POLICY_ALLOW_DEBUG_SHARE enabled)
 set(VCPKG_POLICY_ALLOW_EMPTY_FOLDERS enabled)
